95054cb73443deff9edb883e7f71100bbee9f4f6
METAEGGAS <05318352256bna@gmail.com>
2026-06-03 06:56:24 +0300

    Change injected page from 'withdrawal' to 'record'

--- record.js
+++ record.js
@@ -1,1 +1,1 @@
-injectPage("withdrawal",{v:()=>`<div class=card><h3>${App.t("withdrawal")}</h3><p>demo withdrawal.js loaded dynamically</p></div>`});
+injectPage("record",{v:()=>`<div class=card><h3>${App.t("withdrawal")}</h3><p>demo withdrawal.js loaded dynamically</p></div>`});
